55aa53010d39bc533d967e953f4494947fae23d7
김현진 <khjzzm@gmail.com>
2020-01-13 13:45:59 +0900

    CORS

--- src/main/java/kim/zzang/webservice/web/MemberController.java
+++ src/main/java/kim/zzang/webservice/web/MemberController.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import kim.zzang.webservice.dto.member.MemberMainResponseDto;
 import kim.zzang.webservice.dto.member.MemberSaveRequestDto;
@@ -15,6 +12,7 @@ import kim.zzang.webservice.service.MemberService;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin(origins = {"https://zzang.kim", "https://www.zzang.kim"})
 public class MemberController {
 
     private MemberService memberService;
